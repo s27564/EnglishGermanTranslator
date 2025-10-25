09932818fad2a311583544cab4efbc24b565390e
s27564 <s27564@pjwstk.edu.pl>
2025-10-25 15:23:09 +0200

    different model and tokenizer

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,25 +19,21 @@ if option == options[0]:
     st.image(["Flag_of_the_United_Kingdom_(3-5).svg.png","Germany-Flag.jpg"],width = 100)
     text = st.text_area(label="Wpisz tekst po angielsku:", key=12)
     if text:
-        st.spinner()
         with st.spinner(text='Tłumaczenie...'):
             try:
-                tokenizer = AutoTokenizer.from_pretrained("DunnBC22/mbart-large-50-English_German_Translation")
-                model = AutoModelForSeq2SeqLM.from_pretrained("DunnBC22/mbart-large-50-English_German_Translation")
-
-                tokenizer.src_lang = "en_XX"
-                tokenizer.tgt_lang = "de_DE"
+                tokenizer = AutoTokenizer.from_pretrained("Helsinki-NLP/opus-mt-en-de")
+                model = AutoModelForSeq2SeqLM.from_pretrained("Helsinki-NLP/opus-mt-en-de")
 
                 inputs = tokenizer(text, return_tensors="pt") 
                 generated_ids = model.generate(**inputs, num_beams=4, max_length=100, early_stopping=True)
     
                 answer = tokenizer.batch_decode(generated_ids, skip_special_tokens=True, clean_up_tokenization_spaces=True)[0]
     
+                st.write("**Tłumaczenie:**")
+                st.success(answer)
             except Exception as e:
                 st.error(f"Wystąpił błąd podczas tłumaczenia: {e}")
     
-        st.write("**Tłumaczenie:**")
-        st.success(answer)
      
 elif option == "Wydźwięk emocjonalny tekstu (eng)":
     text = st.text_area(label="Wpisz tekst")
